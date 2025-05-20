a010f95232ad8b4f6bd0aaf2e44d4a435a898bbf
Fernando Sergio <fernandosergio2196@gmail.com>
2025-05-20 07:56:22 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+struct Join{
+    void teste(){
+        int n;
+        do{
+            scanf("%d", &n);
+            char nome;
+            int timeJoin, timeProcess;
+
+            scanf();
+
+        }while(n > 1 && n < 16);
+    }
+};
+
+
+int main(){
+    struct Join entradaUm;
+
+    entradaUm.teste();
+}
