bb8494160f6357259b27315f51d5e61ab5e07f75
Fernando Sergio <fernandosergio2196@gmail.com>
2025-05-22 10:01:40 -0300

    Update main.cpp
    Vi que com fila não faria sentido, estou vendo com vou implementar o vector.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,55 @@
-#include <iostream>
 #include <stdio.h>
 #include <string.h>
-#define TAM 15
+#define MAX 15
 
-struct No{
-  int valor;
-  No* next;
-  
-  No(int valorNo):valor(valorNo), next(nullptr){} // : -> operador de inicialização de membros
-                                            // ...ou lista de inicialização de membros de um construtor.
+struct Processos {
+	char nome[100];
+	int tempoEntrada, tempoProcesso;
 };
 
-struct Fila{
-    
-};
+void desenvolvimento(struct Processos processo[], int *n) {
+	do {
+		printf("Quantas entradas serão? (entre 2 a 15): ");
+		scanf("%d", n);
+		while (getchar() != '\n');
+	} while (*n < 2 || *n > MAX);
 
+	for (int i = 0; i < *n; i++) {
+		printf("Nome do processo %d: ", i + 1);
+		fgets(processo[i].nome, sizeof(processo[i].nome), stdin);
+		processo[i].nome[strcspn(processo[i].nome, "\n")] = '\0'; 
 
-struct Join{
-    void teste(){
-        int n;
-        do{
-            scanf("%d", &n);
-            char nome;
-            int timeJoin, timeProcess;
-            
-            
-        }while(n > 1 && n < 16);
+		printf("Tempo de entrada do processo %d: ", i + 1);
+		scanf("%d", &processo[i].tempoEntrada);
+
+		printf("Tempo de execução do processo %d: ", i + 1);
+		scanf("%d", &processo[i].tempoProcesso);
+		while (getchar() != '\n'); 
+	}
+	//SRT
+	int fila[MAX];
+	int escalonamento[MAX];
+    for(int i = 0;i < *n; i++){
+       if(escalonamento[i] == 0){
+           escalonamento[i] = processo[i].tempoProcesso 
+       }
     }
-};
+}
 
 
-int main(){
-    struct Join entradaUm;
+int main() {
+	struct Processos srt[15];
+	int n, menuOpcao;
 
-    entradaUm.teste();
+	desenvolvimento(srt, &n);
+
+	printf("\nProcessos inseridos:\n");
+	for (int i = 0; i < n; i++) {
+		printf("Nome: %s / Entrada: %d / Duração: %d\n",
+			srt[i].nome,
+			srt[i].tempoEntrada,
+			srt[i].tempoProcesso);
+	}
+
+	return 0;
 }
